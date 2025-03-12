06d338afcfb4269d61ef481733bb039f0d466a96
Alejandroox <darmdz23@gmail.com>
2025-03-12 14:09:16 -0400

    fix bugs
    add dotenv
    add flow (not used)
    fix  version ts 5.8.2 -> 5.5.4

--- base-ts-baileys-memory/src/app.ts
+++ base-ts-baileys-memory/src/app.ts
@@ -1,20 +1,25 @@
 import 'dotenv/config'
-import { createBot, MemoryDB, createProvider } from '@builderbot/bot'
+import { createBot, MemoryDB, createProvider, createFlow,addKeyword } from '@builderbot/bot'
 import { BaileysProvider } from '@builderbot/provider-baileys'
-import flow from './flows';
+import 'dotenv/config';
+
 
 const PORT = process.env.PORT ?? 3001
 
+const welcomeFlow = addKeyword<BaileysProvider, MemoryDB>(['comandoexageradamenteespecifico'])
+    .addAnswer('Ey! welcome')
+    .addAnswer(`Send image from URL`, { media: 'https://i.imgur.com/0HpzsEm.png' })
+
 const main = async () => {
     const provider = createProvider(BaileysProvider)
+    const adapterFlow = createFlow([welcomeFlow])
 
     const { handleCtx, httpServer } = await createBot({
+        flow: adapterFlow,
         database: new MemoryDB(),
-        provider,
-        flow,
+        provider: provider
     })
-
-    httpServer(+PORT)
+    httpServer(3001)
 
     provider.server.post('/v1/messages', handleCtx(async (bot, req, res) => {
         const { number, message } = req.body
